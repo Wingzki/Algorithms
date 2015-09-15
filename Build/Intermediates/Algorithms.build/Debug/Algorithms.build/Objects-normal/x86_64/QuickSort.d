ef789ccee07a8d4bd902bca0d50c6fd5cfa32322
thewingofthesky <thewingofthesky@gmail.com>
2015-09-15 14:49:35 +0800

    删除文件

--- Build/Intermediates/Algorithms.build/Debug/Algorithms.build/Objects-normal/x86_64/QuickSort.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/thewingofthesky/Documents/Wingzki'sCodeLab/Algorithms/Algorithms/QuickSort.m \
-  /Users/thewingofthesky/Documents/Wingzki'sCodeLab/Algorithms/Algorithms/QuickSort.h
--- Build/Intermediates/Algorithms.build/Debug/Algorithms.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/thewingofthesky/Documents/Wingzki'sCodeLab/Algorithms/Algorithms/main.m \
-  /Users/thewingofthesky/Documents/Wingzki'sCodeLab/Algorithms/Algorithms/QuickSort.h
